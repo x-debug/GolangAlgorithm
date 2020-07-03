be9b9a5ef1246777b8492f0e3a64d725d3fa62c3
nbboy <512766594@qq.com>
2020-07-03 11:06:46 +0800

    实现了双向链表

--- datastructs/doubly_linked_list.go
+++ datastructs/doubly_linked_list.go
@@ -1,1 +1,171 @@
 package datastructs
+
+type DoublyLinkedListNode struct {
+	prev *DoublyLinkedListNode
+
+	next *DoublyLinkedListNode
+
+	data interface{}
+}
+
+type DoublyLinkedList struct {
+	head *DoublyLinkedListNode
+
+	tail *DoublyLinkedListNode
+
+	compareFunc CompareFun
+
+	size int
+}
+
+func CreateDoublyLinkedList(compare CompareFun) *DoublyLinkedList {
+	dll := new(DoublyLinkedList)
+	dll.head = nil
+	dll.tail = nil
+	dll.size = 0
+	dll.compareFunc = compare
+	return dll
+}
+
+func (dll *DoublyLinkedList) AddFront(data interface{}) bool {
+	node := new(DoublyLinkedListNode)
+	node.data = data
+	node.prev = nil
+
+	if dll.size == 0 { //it is empty
+		dll.head = node
+		dll.tail = node
+		node.next = nil //pointer to self
+	} else {
+		node.next = dll.head
+		dll.head.prev = node
+		dll.head = node
+	}
+
+	dll.size++
+	return true
+}
+
+func (dll *DoublyLinkedList) AddEnd(data interface{}) bool {
+	node := new(DoublyLinkedListNode)
+	node.data = data
+	node.next = nil
+
+	if dll.size == 0 { //it is empty
+		dll.head = node
+		dll.tail = node
+		node.prev = nil //pointer to self
+	} else {
+		node.prev = dll.tail
+		dll.tail.next = node
+		dll.tail = node
+	}
+
+	dll.size++
+	return true
+}
+
+func (dll *DoublyLinkedList) AddBefore(at *DoublyLinkedListNode, data interface{}) bool {
+	if at == nil {
+		return false
+	}
+
+	node := new(DoublyLinkedListNode)
+	node.data = data
+
+	node.prev = at.prev
+	node.next = at
+
+	if at.prev != nil {
+		at.prev.next = node
+	} else {
+		dll.head = node
+	}
+	at.prev = node
+
+	dll.size++
+	return true
+}
+
+func (dll *DoublyLinkedList) AddAfter(at *DoublyLinkedListNode, data interface{}) bool {
+	if at == nil {
+		return false
+	}
+
+	node := new(DoublyLinkedListNode)
+	node.data = data
+
+	node.next = at.next
+	node.prev = at
+
+	if at.next != nil {
+		at.next.prev = node
+	} else {
+		dll.tail = node
+	}
+	at.next = node
+
+	dll.size++
+	return true
+}
+
+func (dll DoublyLinkedList) IsEmpty() bool {
+	return dll.size == 0
+}
+
+func (dll DoublyLinkedList) Size() int {
+	return dll.size
+}
+
+func (dll *DoublyLinkedList) DeleteNode(at *DoublyLinkedListNode) bool {
+	if dll.IsEmpty() {
+		return false
+	}
+
+	if at == nil {
+		return false
+	}
+
+	if at.prev != nil {
+		at.prev.next = at.next
+	} else { //pointer to head
+		dll.head = at.next
+	}
+
+	if at.next != nil {
+		at.next.prev = at.prev
+	} else { //pointer to next
+		dll.tail = at.prev
+	}
+
+	dll.size--
+	return true
+}
+
+func (dll *DoublyLinkedList) SearchForward(data interface{}) *DoublyLinkedListNode {
+	if dll.IsEmpty() {
+		return nil
+	}
+
+	for p := dll.tail; p != nil; p = p.prev {
+		if dll.compareFunc(p.data, data) {
+			return p
+		}
+	}
+
+	return nil
+}
+
+func (dll *DoublyLinkedList) SearchBackward(data interface{}) *DoublyLinkedListNode {
+	if dll.IsEmpty() {
+		return nil
+	}
+
+	for p := dll.head; p != nil; p = p.next {
+		if dll.compareFunc(p.data, data) {
+			return p
+		}
+	}
+
+	return nil
+}
--- datastructs/doubly_linked_list_test.go
+++ datastructs/doubly_linked_list_test.go
@@ -1,1 +1,229 @@
 package datastructs
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func doublyCompLinkedList(a interface{}, b interface{}) bool {
+	sa, sb := a.(string), b.(string)
+	return strings.Compare(sa, sb) == 0
+}
+
+func TestCreateDoublyLinkedList(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+
+	if sll.size != 0 {
+		t.Error("个数出现了问题")
+	}
+}
+
+func TestDoublyLinkedList_AddFront(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddFront("hello")
+	sll.AddFront("world")
+	sll.AddFront("google")
+	sll.AddFront("sorry")
+	sll.AddFront("baidu")
+
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+}
+
+func TestDoublyLinkedList_AddEnd(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddEnd("hello")
+	sll.AddEnd("world")
+	sll.AddEnd("google")
+	sll.AddEnd("sorry")
+	sll.AddEnd("baidu")
+
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+}
+
+func TestDoublyLinkedList_AddBefore(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddEnd("hello")
+	sll.AddEnd("world")
+	sll.AddEnd("google")
+	sll.AddEnd("sorry")
+	sll.AddEnd("baidu")
+
+	//hello world google sorry baidu
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+
+	sll.AddBefore(sll.head, "fuck")
+	sll.AddBefore(sll.head, "man")
+	sll.AddBefore(sll.head, "women")
+
+	if sll.Size() != 8 {
+		t.Error("个数出现了问题")
+	}
+
+	fmt.Println("============================")
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+}
+
+func TestDoublyLinkedList_AddAfter(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddEnd("hello")
+	sll.AddEnd("world")
+	sll.AddEnd("google")
+	sll.AddEnd("sorry")
+	sll.AddEnd("baidu")
+
+	//hello world google sorry baidu
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+
+	sll.AddAfter(sll.tail, "fuck")
+	sll.AddAfter(sll.tail, "man")
+	sll.AddAfter(sll.tail, "women")
+
+	if sll.Size() != 8 {
+		t.Error("个数出现了问题")
+	}
+
+	fmt.Println("============================")
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+	doubleOrderNodes([]string{"hello", "world", "google", "sorry", "baidu", "fuck", "man", "women"}, sll, t)
+}
+
+func TestDoublyLinkedList_DeleteNode(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddEnd("hello")
+	sll.AddEnd("world")
+	sll.AddEnd("google")
+	sll.AddEnd("sorry")
+	sll.AddEnd("baidu")
+
+	//hello world google sorry baidu
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+
+	sll.DeleteNode(sll.head)
+	sll.DeleteNode(sll.tail)
+	sll.DeleteNode(sll.tail)
+
+	if sll.Size() != 2 {
+		t.Error("个数出现了问题")
+	}
+
+	fmt.Println("============================")
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+	doubleOrderNodes([]string{"world", "google", "sorry"}, sll, t)
+}
+
+func TestDoublyLinkedList_SearchForward(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddEnd("hello")
+	sll.AddEnd("world")
+	sll.AddEnd("google")
+	sll.AddEnd("sorry")
+	sll.AddEnd("world")
+	sll.AddEnd("baidu")
+
+	//hello world google sorry baidu
+	if sll.Size() != 6 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+
+	node := sll.SearchBackward("world")
+	sll.DeleteNode(node)
+
+	//hello world google sorry baidu
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+	doubleOrderNodes([]string{"hello", "google", "sorry", "world", "baidu"}, sll, t)
+}
+
+func TestDoublyLinkedList_SearchBackward(t *testing.T) {
+	sll := CreateDoublyLinkedList(doublyCompLinkedList)
+	sll.AddEnd("hello")
+	sll.AddEnd("world")
+	sll.AddEnd("google")
+	sll.AddEnd("sorry")
+	sll.AddEnd("world")
+	sll.AddEnd("baidu")
+
+	//hello world google sorry baidu
+	if sll.Size() != 6 {
+		t.Error("个数出现了问题")
+	}
+
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+
+	node := sll.SearchForward("world")
+	sll.DeleteNode(node)
+
+	//hello world google sorry baidu
+	if sll.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+	doublyLookup(sll, t)
+	doublyLookNodes(sll, t)
+	doubleOrderNodes([]string{"hello", "world", "google", "sorry", "baidu"}, sll, t)
+}
+
+//此函数为了测试链表的连续性
+func doublyLookup(sll *DoublyLinkedList, t *testing.T) {
+	var i int = 0
+
+	for p := sll.head; p != nil; p = p.next {
+		i++
+	}
+
+	if i != sll.Size() {
+		t.Errorf("迭代不连续, i:%d, Size():%d", i, sll.Size())
+	}
+}
+
+func doublyLookNodes(sll *DoublyLinkedList, t *testing.T) {
+	for p := sll.head; p != nil; p = p.next {
+		fmt.Println(p.data)
+	}
+}
+
+func doubleOrderNodes(right []string, sll *DoublyLinkedList, t *testing.T) {
+	var i int = 0
+	for p := sll.head; p != nil; p = p.next {
+		if right[i] != p.data {
+			t.Error("元素次序不对")
+		}
+
+		i++
+	}
+}
