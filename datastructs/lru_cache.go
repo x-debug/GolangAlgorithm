5a0312d798cb9e9a694ac00576379039ef1bb8d1
nbboy <512766594@qq.com>
2020-07-03 14:51:21 +0800

    add lru cache

--- datastructs/lru_cache.go
+++ datastructs/lru_cache.go
@@ -1,1 +1,90 @@
 package datastructs
+
+//简单LRU Cache 缓存淘汰算法
+type LRUElement struct {
+	ptr *DoublyLinkedListNode
+}
+
+type LRULinkedElement struct {
+	key  string
+	data interface{}
+}
+
+type LRUCache struct {
+	size      int
+	maxSize   int
+	cacheList *DoublyLinkedList
+	cacheMap  map[string]*LRUElement
+}
+
+func LRUCacheKeyCompare(a interface{}, b interface{}) bool {
+	//ignore
+	return true
+}
+
+func CreateLRUCache(maxSize int) *LRUCache {
+	lru := new(LRUCache)
+	lru.maxSize = maxSize
+	lru.size = 0
+	lru.cacheList = CreateDoublyLinkedList(LRUCacheKeyCompare)
+	lru.cacheMap = make(map[string]*LRUElement)
+	return lru
+}
+
+func (lru LRUCache) Get(key string) (interface{}, bool) {
+	ele, ok := lru.cacheMap[key]
+
+	if ok {
+		//放到头结点上去
+		lru.cacheList.DeleteNode(ele.ptr)
+		lru.cacheList.AddFrontAt(ele.ptr)
+		return ele.ptr.data, true
+	}
+
+	return nil, false
+}
+
+func (lru *LRUCache) Set(key string, value interface{}) bool {
+	ele, ok := lru.cacheMap[key]
+
+	if ok {
+		lru.cacheList.DeleteNode(ele.ptr)
+	} else {
+		//淘汰最近没访问过的数据
+		lru.size++
+		if lru.size > lru.maxSize {
+			delete(lru.cacheMap, lru.cacheList.tail.data.(*LRULinkedElement).key)
+			lru.cacheList.DeleteNode(lru.cacheList.tail)
+			lru.size--
+		}
+		ele = new(LRUElement)
+		listNode := &LRULinkedElement{
+			data: value, key: key,
+		}
+		ele.ptr = CreateDoublyLinkedListNode(listNode)
+	}
+
+	item := ele.ptr.data.(*LRULinkedElement)
+	item.data = value
+	lru.cacheList.AddFrontAt(ele.ptr)
+	ele.ptr = lru.cacheList.head
+	lru.cacheMap[key] = ele
+	return true
+}
+
+func (lru *LRUCache) Del(key string) bool {
+	ele, ok := lru.cacheMap[key]
+
+	if ok {
+		lru.cacheList.DeleteNode(ele.ptr)
+		delete(lru.cacheMap, key)
+		lru.size--
+		return true
+	}
+
+	return false
+}
+
+func (lru LRUCache) Size() int {
+	return lru.size
+}
--- datastructs/lru_cache_test.go
+++ datastructs/lru_cache_test.go
@@ -1,1 +1,106 @@
 package datastructs
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCreateLRUCache(t *testing.T) {
+	lru := CreateLRUCache(5)
+
+	lru.Set("hello1", "11")
+	lru.Set("hello2", "设计")
+	lru.Set("hello3", "100")
+	lru.Set("hello4", "产品")
+	lru.Set("hello5", "19")
+	lru.Set("hello6", "需求")
+
+	//limit to 5
+	if lru.Size() != 5 {
+		t.Errorf("个数出现了问题, Size():%d", lru.Size())
+	}
+}
+
+func TestLRUCache_Get(t *testing.T) {
+	lru := CreateLRUCache(5)
+
+	lru.Set("hello1", "11")
+	lru.Set("hello2", "设计")
+	lru.Set("hello3", "100")
+	lru.Set("hello4", "产品")
+	lru.Set("hello5", "19")
+	lru.Set("hello6", "需求")
+
+	//limit to 5
+	if lru.Size() != 5 {
+		t.Errorf("个数出现了问题, Size():%d", lru.Size())
+	}
+
+	value, ok := lru.Get("hello3")
+	if ok {
+		fmt.Printf("Value:%s\r\n", value)
+	}
+
+	lruLoopUp(lru, t)
+	fmt.Println(lru.cacheMap)
+	if lru.cacheList.head.data.(*LRULinkedElement).key != "hello3" {
+		t.Error("Get顺序出现了问题")
+	}
+}
+
+func TestLRUCache_Set(t *testing.T) {
+	lru := CreateLRUCache(5)
+
+	lru.Set("hello1", "11")
+	lru.Set("hello2", "设计")
+	lru.Set("hello3", "100")
+	lru.Set("hello4", "产品")
+	lru.Set("hello5", "19")
+	lru.Set("hello6", "需求")
+
+	//limit to 5
+	if lru.Size() != 5 {
+		t.Errorf("个数出现了问题, Size():%d", lru.Size())
+	}
+
+	lruLoopUp(lru, t)
+	fmt.Println(lru.cacheMap)
+	if v, ok := lru.cacheMap["hello1"]; ok {
+		t.Errorf("Set出现了问题, %s Len():%d", v.ptr.data, lru.cacheList.Size())
+	}
+}
+
+func TestLRUCache_Del(t *testing.T) {
+	lru := CreateLRUCache(5)
+
+	lru.Set("hello1", "11")
+	lru.Set("hello2", "设计")
+	lru.Set("hello3", "100")
+	lru.Set("hello4", "产品")
+	lru.Set("hello5", "19")
+	lru.Set("hello6", "需求")
+
+	//limit to 5
+	if lru.Size() != 5 {
+		t.Errorf("个数出现了问题, Size():%d", lru.Size())
+	}
+
+	lruLoopUp(lru, t)
+	fmt.Println(lru.cacheMap)
+
+	lru.Del("hello2")
+	lru.Del("hello4")
+
+	if lru.Size() != 3 {
+		t.Errorf("个数出现了问题, Size():%d", lru.Size())
+	}
+
+	lruLoopUp(lru, t)
+	fmt.Println(lru.cacheMap)
+}
+
+func lruLoopUp(lru *LRUCache, t *testing.T) {
+	for p := lru.cacheList.head; p != nil; p = p.next {
+		fmt.Println(p.data)
+	}
+}
