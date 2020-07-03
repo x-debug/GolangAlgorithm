354e395d7bc783b387af0c0d501ce91c47fff9d6
nbboy <512766594@qq.com>
2020-07-03 21:17:31 +0800

    add stack

---
+++ datastructs/stack.go
@@ -0,0 +1,34 @@
+package datastructs
+
+type Stack struct {
+	impl *SinglyLinkedList
+}
+
+func CreateStack() *Stack {
+	s := new(Stack)
+	s.impl = CreateLinkedList(nil)
+	return s
+}
+
+func (s *Stack) Push(data interface{}) bool {
+	s.impl.AddAtFront(data)
+	return true
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	data := s.impl.head.data
+	s.impl.DeleteElement(s.impl.head)
+
+	return data
+}
+
+func (s Stack) IsEmpty() bool {
+	return s.Size() == 0
+}
+
+func (s Stack) Size() int {
+	return s.impl.Size()
+}
---
+++ datastructs/stack_test.go
@@ -0,0 +1,58 @@
+package datastructs
+
+import "testing"
+
+func TestCreateStack(t *testing.T) {
+	stack := CreateStack()
+
+	if !stack.IsEmpty() {
+		t.Error("个数出现了问题")
+	}
+}
+
+func TestStack_Push(t *testing.T) {
+	stack := CreateStack()
+
+	stack.Push("a")
+	stack.Push("b")
+	stack.Push("c")
+	stack.Push("d")
+	stack.Push("e")
+
+	if stack.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	stackOrderNodes([]string{"e", "d", "c", "b", "a"}, stack, t)
+}
+
+func TestStack_Pop(t *testing.T) {
+	stack := CreateStack()
+
+	stack.Push("a")
+	stack.Push("b")
+	stack.Push("c")
+	stack.Push("d")
+	stack.Push("e")
+
+	if stack.Size() != 5 {
+		t.Error("个数出现了问题")
+	}
+
+	stackOrderNodes([]string{"e", "d", "c", "b", "a"}, stack, t)
+
+	stack.Pop()
+	stack.Pop()
+	stackOrderNodes([]string{"c", "b", "a"}, stack, t)
+}
+
+func stackOrderNodes(right []string, s *Stack, t *testing.T) {
+	var i int = 0
+	for p := s.impl.head; p != nil; p = p.next {
+		if right[i] != p.data {
+			t.Error("元素次序不对")
+		}
+
+		i++
+	}
+}
